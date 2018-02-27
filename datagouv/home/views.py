21d7a15ef0a4f02127152479db9b24bf3baad36b
dargrosion <dargrosion@fatlife.gouv.fr>
2018-02-27 15:31:36 +0100

    Views modifications

--- datagouv/home/views.py
+++ datagouv/home/views.py
@@ -8,6 +8,13 @@ def home(request):
 
     return render(request, "base.html", locals())
 
+def description(request):
+
+    return render(request, "description.html", locals())
+
+def recherche(request):
+    return render(request, "recherche.html", locals())
+
 def renduEcoleDocto(request, ecoleid=0):
     ret = EcoleDoctorante.objects.get(numero=ecoleid)
     return render(request, "IdEcoleDoctorante.html", locals())
