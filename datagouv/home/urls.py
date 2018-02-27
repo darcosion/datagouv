57d97662a7f0913ce1e2dd8af1a01a3a0f763068
Ahmed Haouili <ahmed_haouili@yahoo.fr>
2018-02-27 15:31:36 +0100

    Views modifications

--- datagouv/home/urls.py
+++ datagouv/home/urls.py
@@ -6,5 +6,7 @@ app_name = 'home'
 urlpatterns = [
     url(r'^ecole/(?P<ecoleid>\d+)$', views.renduEcoleDocto, name='idecole'),
     url(r'^$', views.home, name='home'),
+    url(r'description/$', views.description, name='description'),
+    url(r'recherche/$', views.recherche, name='recherche'),
 ]
 
