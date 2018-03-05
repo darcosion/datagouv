c459828956e7b5151db6fa2f40b80921d1bd553d
dargrosion <dargrosion@fatlife.gouv.fr>
2018-03-05 20:42:19 +0100

    model propre

--- datagouv/home/effectif_regional.py
+++ datagouv/home/effectif_regional.py
@@ -1,12 +1,14 @@
-import csv
+import csv, os
 
-from .models import EcoleDoctorante
+from .models import EffectifRegional
 
 # attention, le délimiteur est sensé être un ";" mais
 # s'il y a des doubles quotes, il ne doit pas être pris en compte
 # dedans...
 
-with open('fr-esr-ecoles_doctorales_annuaire.csv') as csvfile:
+compt = 1
+
+with open('../fr-esr-atlas_regional-effectifs-d-etudiants-inscrits.csv') as csvfile:
     # le delimiter permet de définir les entrée
     # le quotechar sépare les colonnes
     reader = csv.reader(csvfile, delimiter='\n')
@@ -14,7 +16,8 @@ with open('fr-esr-ecoles_doctorales_annuaire.csv') as csvfile:
         trow = list(csv.reader(row, delimiter=';'))
         if(trow[0][10] == ''):
             trow[0][10] = None
-        t = EcoleDoctorante(trow[0][0]
+        t = EffectifRegional(True
+            ,trow[0][0]
 ,trow[0][1]
 ,trow[0][2]
 ,trow[0][3]
@@ -39,24 +42,12 @@ with open('fr-esr-ecoles_doctorales_annuaire.csv') as csvfile:
 ,trow[0][22]
 ,trow[0][23]
 ,trow[0][24]
-,trow[0][25]
-,trow[0][26]
-,trow[0][27]
-,trow[0][28]
-,trow[0][29]
-,trow[0][30]
-,trow[0][31]
-,trow[0][32]
-,trow[0][33]
-,trow[0][34]
-,trow[0][35]
-,trow[0][36]
-,trow[0][37]
-,trow[0][38]
-,trow[0][39]
-,trow[0][40]
-,trow[0][41]
-,trow[0][42]
-,trow[0][43])
-        t.save()
+)
+        try:
+            t.save()
+        except Exception as e:
+            print(t)
+            print(e)
+            break
+        compt+=1
 
